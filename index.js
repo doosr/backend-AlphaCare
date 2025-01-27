bb1d32307147e1bea58097f4ec7681022fcf5621
dawser <dawserbelgacem122@gmail.com>
2025-01-27 17:43:49 +0100

    2sdd

--- index.js
+++ index.js
@@ -545,7 +545,7 @@ app.get('/temperature/:babyId', (req, res) => {
        
 // Démarrage du serveur sur un port spécifique
 const PORT = process.env.PORT || 5000;
-server.listen(PORT,'0.0.0.0', () => {
+server.listen(PORT,/*'0.0.0.0',*/ () => {
     console.log(`Serveur démarré sur le port ${PORT}`);
 });
 
